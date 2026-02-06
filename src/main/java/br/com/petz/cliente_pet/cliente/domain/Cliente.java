05e5048b4fa44f174da2c336bd96617467e108e8
Roberto Pedro <robertopedro100@gmail.com>
2026-02-06 20:10:44 -0300

    Implementa classe dominio Cliente

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
@@ -0,0 +1,57 @@
+package br.com.petz.cliente_pet.cliente.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID idCliente;
+    @NotBlank
+    private String nomeCompleto;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    @Enumerated(EnumType.STRING)
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
+
+    private LocalDateTime dataHoraCadastro;
+    private LocalDateTime dataHoraDaUltimaAlteracao;
+
+    public Cliente(UUID idCliente, String nomeCompleto, String email, String celular, String telefone,
+                   Sexo sexo, LocalDate dataNascimento, String cpf, Boolean aceitaTermos, LocalDateTime dataHoraCadastro,
+                   LocalDateTime dataHoraDaUltimaAlteracao) {
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.aceitaTermos = aceitaTermos;
+        this.dataHoraCadastro = LocalDateTime.now();
+    }
+}
