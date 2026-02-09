6b3122b61167a07b2291682ac88fc84605888c49
Roberto Pedro <robertopedro100@gmail.com>
2026-02-09 02:34:01 -0300

    adiciona column definition na classe Cliente

--- src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/domain/Cliente.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    @Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
     private UUID idCliente;
     @NotBlank
     private String nomeCompleto;
