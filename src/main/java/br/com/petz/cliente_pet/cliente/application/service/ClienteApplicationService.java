dd7c1988389664a0071484790e7037de171960ab
Roberto Pedro <robertopedro100@gmail.com>
2026-03-04 15:18:30 -0300

    implementa contrutor para Clienterequest e implementa fluxo repository

--- src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
@@ -2,18 +2,24 @@ package br.com.petz.cliente_pet.cliente.application.service;
 
 import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
+import br.com.petz.cliente_pet.cliente.application.repository.ClienteRepository;
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteApplicationService implements ClienteService {
-
+    private final ClienteRepository clienteRepository;
 
     @Override
     public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
         log.info("[inicia] ClienteApplicationService - criaCliente");
+        Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
         log.info("[finaliza] ClienteApplicationService - criaCliente");
-        return null;
+        return ClienteResponse.builder().idCliente(cliente.getIdCliente())
+                .build();
     }
 }
