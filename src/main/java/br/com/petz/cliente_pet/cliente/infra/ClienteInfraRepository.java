a317e97a29bc9178aaa0a07a7d479ef42c5e4b3b
Roberto Pedro <robertopedro100@gmail.com>
2026-03-09 22:45:53 -0300

    implementa Repository e interface ClienteSpringDatajparepository

--- src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/infra/ClienteInfraRepository.java
@@ -2,17 +2,21 @@ package br.com.petz.cliente_pet.cliente.infra;
 
 import br.com.petz.cliente_pet.cliente.application.repository.ClienteRepository;
 import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.antlr.v4.runtime.misc.LogManager;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
-
+    private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
 
     @Override
     public Cliente salva(Cliente cliente) {
         log.info("[inicia] ClienteInfraRepository - salva");
+        clienteSpringDataJPARepository.save(cliente);
         log.info("[finaliza] ClienteInfraRepository - salva");
         return cliente;
     }
