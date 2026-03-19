fe9abd14c05aa88d3b9cf6092dc76f0a33b1e58e
Roberto Pedro <robertopedro100@gmail.com>
2026-03-19 19:47:46 -0300

    Implementa Get-Cliente-Por-Id + service e repository

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteDetalhadoResponse.java
@@ -1,6 +1,8 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
 import br.com.petz.cliente_pet.cliente.domain.Sexo;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Value;
 
 import java.time.LocalDate;
@@ -8,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Value
+@JsonPropertyOrder({"idCliente", "nomeCompleto", "cpf","email" ,"celular","telefone","sexo", "dataNascimento"})
 public class ClienteDetalhadoResponse {
     private UUID idCliente;
     private String nomeCompleto;
@@ -20,4 +23,18 @@ public class ClienteDetalhadoResponse {
     private Boolean aceitaTermos;
 
     private LocalDateTime dataHoraCadastro;
+
+    public ClienteDetalhadoResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.celular = cliente.getCelular();
+        this.telefone = cliente.getTelefone();
+        this.sexo = cliente.getSexo();
+        this.dataNascimento = cliente.getDataNascimento();
+        this.aceitaTermos = cliente.getAceitaTermos();
+        this.dataHoraCadastro = cliente.getDataHoraCadastro();
+    }
+
 }
