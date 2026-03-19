17c6b85758291527bc692309c7e3ce0cdc9d8dae
Roberto Pedro <robertopedro100@gmail.com>
2026-03-19 19:06:16 -0300

    Implementa fluxo do service

--- src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteService.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteService.java
@@ -1,13 +1,17 @@
 package br.com.petz.cliente_pet.cliente.application.service;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteListResponse;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
 import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ClienteService {
     ClienteResponse criaCliente(ClienteRequest clienteRequest);
 
     List<ClienteListResponse> buscaTodosClientes();
+
+    ClienteDetalhadoResponse buscaClienteAtravesId(UUID idCliente);
 }
